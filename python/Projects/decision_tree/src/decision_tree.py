6f56ed1957ef3c2ab7501e0b8cc9a3b8c64af98c
makao007@126.com <makao007@126.com>
2013-10-14 20:13:52 +0800

    decistion tree , update

--- python/Projects/decision_tree/src/decision_tree.py
+++ python/Projects/decision_tree/src/decision_tree.py
@@ -35,11 +35,37 @@ def dataset_entropy (dataset, value=None, column=None):
     return entropy (labels, dataset_length)
 
 def max_info_gain (dataset):
-    
+    columns = len (dataset[0]) -1 
+    length = len (dataset)
+
+    max_info_gain_index = -1
+    max_info_gain_value = -1
+    for i in range (columns):
+        features = [line[i] for line in dataset]
+        features_dict = {}
+        for feature in features:
+            if not features_dict.has_key(feature):
+                features_dict[feature] = 0
+            features_dict[feature] += 1
+
+        entropy = 0
+        for key,value in features_dict.items():
+            entropy += float(value)/length * dataset_entropy(dataset, key, i)
+
+        if entropy > max_info_gain_value :
+            max_info_gain_value = entropy
+            max_info_gain_index = i
+
+    return max_info_gain_index
+        
 
 def _test_entropy ():
-    print dataset_entropy([[1,1,'yes'],[1,1,'yes'],[1,0,'no'],[0,1,'no'],[0,1,'no']])
-    print dataset_entropy (load_dataset())
+    dataset1 = [[1,1,'yes'],[1,1,'yes'],[1,0,'no'],[0,1,'no'],[0,1,'no']]
+    dataset2 = load_dataset()
+    print dataset_entropy (dataset1)
+    print dataset_entropy (dataset2)
+
+    print max_info_gain (dataset2)
 
 _test_entropy()
 
